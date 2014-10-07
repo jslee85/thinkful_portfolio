929760d60ad1168e3b04e41516c99e4405ec13e5
Jonathan Lee <Jonathan@macmini.omghax.biz>
2014-10-06 22:06:34 -0700

    add ids

--- js/app.js
+++ js/app.js
@@ -1,2 +1,11 @@
 $(document).ready(function () {
+	$(function() {
+		$('a.page-scroll').on('click', function(event) {
+			var $anchor = $(this);
+			$('html, body').stop().animate({
+				scrollTop: $($anchor.attr('href')).offset().top
+			}, 1500, 'easeInOutExpo');
+			event.preventDefault();
+		});
+	});
 });
